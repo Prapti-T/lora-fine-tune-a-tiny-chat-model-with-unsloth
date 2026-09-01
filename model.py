de3568dc4de9eea7806b701ab39e95041813faf8
Prapti-T <85100556+Prapti-T@users.noreply.github.com>
2026-09-01 16:59:54 +0545

    Step 16: build_sft_trainer

--- model.py
+++ model.py
@@ -167,7 +167,6 @@ def build_training_arguments(output_dir='./sft_out', max_steps=5, learning_rate=
 # Step 16 - build_sft_trainer
 from trl import SFTTrainer
 
-
 def build_sft_trainer(model, tokenizer, dataset, training_args, max_seq_length=256):
     """Construct a trl SFTTrainer over dataset['text'] ready to .train()."""
     # TODO: wire model, tokenizer, dataset, and training_args into an SFTTrainer
